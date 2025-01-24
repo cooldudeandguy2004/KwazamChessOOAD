461a71b8db4877eda8fcbe19220437f5cd0e86ba
AdlanAlias <adlanalias167@gmail.com>
2025-01-24 16:35:47 +0800

    Add comment

--- src/main/Biz.java
+++ src/main/Biz.java
@@ -11,10 +11,12 @@ public class Biz extends Piece {
             image = getImage("res\\Piece\\RBIZ.png");
         }
     }
+
     public boolean canMove(int targetCol, int targetRow) {
-        if(isWithinBoard(targetCol, targetRow)) {
-            if(Math.abs(targetCol - preCol) * Math.abs(targetRow - preRow) == 2) {
-                if(isValidSquare(targetCol, targetRow)) {
+        if (isWithinBoard(targetCol, targetRow)) {
+            // Biz moves in a 3x2 L shape in any orientation (like a knight in standard chess)
+            if (Math.abs(targetCol - preCol) * Math.abs(targetRow - preRow) == 2) {
+                if (isValidSquare(targetCol, targetRow)) {
                     return true;
                 }
             }
