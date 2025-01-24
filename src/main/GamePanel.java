af52eb94aff798b15588802050a3187c97780fc2
Ammar <ammarmuqriz6555@gmail.com>
2025-01-24 15:01:04 +0800

    movement of pieces

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -2,7 +2,6 @@ package main;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -15,7 +14,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
